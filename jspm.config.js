1c7717de8ff13ec04561320002294936715026ac
Felix Rabe <felix@rabe.io>
2017-08-26 15:32:25 +0200

    jspm

--- jspm.config.js
+++ jspm.config.js
@@ -1,25 +1,10 @@
 SystemJS.config({
   paths: {
-    "npm:": "jspm_packages/npm/",
-    "ii/": "src/"
-  },
-  browserConfig: {
-    "baseURL": "/"
+    "npm:": "jspm_packages/npm/"
   },
   devConfig: {
     "map": {
       "plugin-babel": "npm:systemjs-plugin-babel@0.0.25"
-    }
-  },
-  transpiler: "plugin-babel",
-  packages: {
-    "ii": {
-      "main": "ii.js",
-      "meta": {
-        "*.js": {
-          "loader": "plugin-babel"
-        }
-      }
     }
   }
 });
