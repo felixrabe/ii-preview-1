e4fdae76b3106ed4bf28f10d8ca25494f8629c55
Felix Rabe <felix@rabe.io>
2017-08-30 00:11:08 +0200

    AppLoader config

--- src/browser/AppLoader.js
+++ src/browser/AppLoader.js
@@ -1,4 +1,20 @@
 const AppLoader = new SystemJS.constructor()
 
+AppLoader._configured = (async () => {
+  const sysCfg = SystemJS.getConfig()
+  const iiPkgUrl = SystemJS.resolveSync('ii').replace(/\/[^/]+$/, '')
+  const iiPkg = sysCfg.packages[iiPkgUrl]
+  AppLoader.config({
+    paths: {
+      'ii/': sysCfg.baseURL + sysCfg.paths['ii/'],
+    },
+    packages: {
+      ii: iiPkg,
+    },
+  })
+
+  await AppLoader.import('/jspm.config.js')
+})()
+
 export const __useDefault = AppLoader
 export default __useDefault
