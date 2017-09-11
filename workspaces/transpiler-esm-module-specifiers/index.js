7c52b8607574aff4446634c776884e07a06bf8b9
Felix Rabe <felix@rabe.io>
2017-09-11 23:04:50 +0200

    wip

--- workspaces/transpiler-esm-module-specifiers/index.js
+++ workspaces/transpiler-esm-module-specifiers/index.js
@@ -1,1 +1,28 @@
-module.exports = (code) => code
+const jscodeshift = require('jscodeshift')
+
+const j = jscodeshift
+
+module.exports = (code) => {
+  const i = []
+
+  code = j(code)
+    .find(j.ImportDeclaration)
+    .forEach(p => {
+      i.push(p)
+    })
+    .toSource()
+
+  return i.map(x => `${x.name} ${x.value.type} ${typeof x.value.type} ${Object.keys(x.value.type)}`).join('\n')
+  return code
+
+  // .find(j.ImportDeclaration).forEach(p => {console.log('x')})
+
+  // const i = []
+  // code = j(code)
+  //   .find(j.ImportStatement)
+  //   .forEach(p => {
+  //     i.push(p)
+  //   })
+  //   .toSource()
+  // return i.map(x => JSON.stringify(x)).join('\n')
+}
