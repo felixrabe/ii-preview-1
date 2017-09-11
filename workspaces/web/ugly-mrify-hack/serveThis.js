ff29753af3a055f81921ef9cdb3829a3ce3e7ec8
Felix Rabe <felix@rabe.io>
2017-09-11 19:31:27 +0200

    wip

--- workspaces/web/ugly-mrify-hack/serveThis.js
+++ workspaces/web/ugly-mrify-hack/serveThis.js
@@ -1,15 +1,18 @@
 const connect = require('connect')
 const path = require('path')
+const send = require('send')
 const serveStatic = require('serve-static')
 
 const nodeModulesRootPath = path.resolve(__dirname, '../../../node_modules')
 const rootStatic = serveStatic(nodeModulesRootPath, {index: false})
+const indexHtmlPath = path.join(__dirname, 'index.html')
 
 module.exports = (() => {
   const router = connect()
 
   router.use('/node_modules', rootStatic)
   router.use(serveStatic(__dirname, {index: false}))
+  router.use((req, res) => send(req, indexHtmlPath).pipe(res))
 
   return router
 })()
