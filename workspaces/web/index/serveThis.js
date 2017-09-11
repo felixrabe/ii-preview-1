6f7ceee6a51acb969ff8d5ba2e38a0b395cf2f62
Felix Rabe <felix@rabe.io>
2017-09-11 19:09:59 +0200

    wip

--- workspaces/web/index/serveThis.js
+++ workspaces/web/index/serveThis.js
@@ -1,18 +1,6 @@
-const connect = require('connect')
 const path = require('path')
 const send = require('send')
 
 const indexHtmlPath = path.join(__dirname, 'index.html')
 
 module.exports = (req, res) => send(req, indexHtmlPath).pipe(res)
-
-// const nodeModules = require('ii-1-web-node-modules')
-
-// module.exports = (() => {
-//   const route = connect()
-
-//   route.use(nodeModules(__dirname))
-//   route.use((req, res) => send(req, indexHtmlPath).pipe(res))
-
-//   return route
-// })()
