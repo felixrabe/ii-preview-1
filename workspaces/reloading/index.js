2183bb37f6e16679d861514dd6a0aedbc7150877
Felix Rabe <felix@rabe.io>
2017-09-24 12:40:46 +0200

    reloading

---
+++ workspaces/reloading/index.js
@@ -0,0 +1,26 @@
+// app.js:        app.use('/foo', reloading(() => require('foo')))
+// foo/index.js:  module.exports = (req, res, next) => { ... }
+
+module.exports = (cb) => {
+  const waitMS = 1000
+  let lastRequest = undefined
+  let handler = undefined
+
+  return (req, res, next) => {
+    if (!(lastRequest + waitMS > Date.now())) {
+      Object.keys(require.cache).forEach(k => {
+        delete require.cache[k]
+      })
+
+      try {
+        handler = cb()
+      } catch (err) {
+        handler = (req, res, next) => {
+          return next(err)
+        }
+      }
+    }
+    lastRequest = Date.now()
+    return handler(req, res, next)
+  }
+}
