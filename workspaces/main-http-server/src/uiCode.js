8180e05db8d9c8341e5c27d94e772d57602251c7
Felix Rabe <felix@rabe.io>
2017-09-24 13:02:30 +0200

    wip2/main-http-server

--- workspaces/main-http-server/src/uiCode.js
+++ workspaces/main-http-server/src/uiCode.js
@@ -1,42 +1,61 @@
+const parseUrl = require('parseurl')
+
 const moduleLocator = require('ii-module-locator')
 
 const uiMod = require('../ui/_thisMod')
 const babel = require('./babel')
 
+const jstr = o => JSON.stringify(o, null, 2)
 const loc = moduleLocator(uiMod)
 
+const getRoute = (req) => {
+  let orig = parseUrl.original(req).pathname
+  if (orig.endsWith('/')) orig = orig.slice(0, -1)
+  const url = parseUrl(req).pathname
+  if (url === '/') return orig
+  return orig.slice(0, -url.length)
+}
+
 module.exports = async (req, res, next) => {
-
-
-
-
-
-
-
-
-
-
-
-
-
-  const resolved = resolveToFileOrRedirect(req)
-
-  if (resolved.indexOf('/') === -1) {
-    // res.writeHead(302, {Location: 'foo/bar/baz'})
-    return res.end()
-  }
-
-  return res.end(jstr({
-    resolved,
-  }))
-
   try {
-    const code = await babel(filePath)
+    const {filePath, modName, modType, redirect} = loc(req)
 
+    if (redirect) {
+      res.writeHead(302, {Location: getRoute(req) + redirect})
+      return res.end()
+    }
+
+    if (!filePath) {
+      return next()
+    }
+
+    const code = await babel(filePath)
     res.writeHead(200, {'Content-Type': 'application/javascript'})
     return res.end(code)
   } catch (err) {
-    res.writeHead(500, {'Content-Type': 'text/plain; charset=utf-8'})
-    return res.end(jstr({error: {name: err.name, message: err.message}}))
+    return next(err)
   }
 }
+
+// module.exports = async (req, res, next) => {
+//   let filePath, modName, modType, redirect
+//   try {
+//     ({filePath, modName, modType, redirect} = loc(req))
+//   } catch (err) {
+//     return next(err)
+//   }
+
+//   if (redirect) {
+//     res.writeHead(302, {Location: getRoute(req) + redirect})
+//     return res.end()
+//   }
+
+//   let code
+//   try {
+//     code = await babel(filePath)
+//   } catch (err) {
+//     return next(err)
+//   }
+//   res.writeHead(200, {'Content-Type': 'application/javascript'})
+//   return res.end(code)
+// }
--- workspaces/main-http-server/ui/index.mjs
+++ workspaces/main-http-server/ui/index.mjs
@@ -1,2 +1,12 @@
 import React from 'react'
+import ReactDOM from 'react-dom'
 import Redux from 'redux'
+
+import App from './components/App.mjs'
+
+document.getElementById('root').textContent = ''
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root'),
+)
